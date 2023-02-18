c6b219b85e31261d0d58608614b73d78babadc54
maxefimov <efimov.academy.top@gmail.com>
2023-02-18 13:21:18 +0300

    Добавьте файлы проекта.

---
+++ FPDebuggingProject/FPDebuggingProject.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    int* ptr = new int;
+
+    int n = 23;
+
+    int size;
+    cout << "input size of array: ";
+    cin >> size;
+
+    int* array = new int[size];
+
+    delete[] array;
+
+    array = new int[size * 2];
+
+    float x = 10;
+    float* fptr = &x;
+    int* iptr;
+    void* vptr = fptr;
+    iptr = (int*)vptr;
+
+    cout << *iptr << "\n";
+}
